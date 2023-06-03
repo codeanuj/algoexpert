95be7c17d80324a7fe67b0296524235d2a5e2443
Anuj <80551063+codeanuj@users.noreply.github.com>
2023-06-03 23:04:52 +0530

    Update TournamentWinner.java

--- Array/Tournament WInner/TournamentWinner.java
+++ Array/Tournament WInner/TournamentWinner.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+class Program {
+
+  public String tournamentWinner(
+      ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+    HashMap<String,Integer> map = new HashMap<>();
+    String team, winner="";
+    for(int i=0; i<results.size(); i++){
+      team = competitions.get(i).get(results.get(i) == 0 ? 1 : 0);
+      map.put(team, map.getOrDefault(team, 0) + 3);
+      winner = map.get(team)>map.getOrDefault(winner, 0) ? team : winner;
+    }
+    return winner;
+  }
+}
