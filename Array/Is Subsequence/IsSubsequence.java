e3f8fa7926c77246c166752041a58386f1b6446e
Anuj <80551063+codeanuj@users.noreply.github.com>
2023-06-03 15:56:19 +0530

    Update IsSubsequence.java

--- Array/Is Subsequence/IsSubsequence.java
+++ Array/Is Subsequence/IsSubsequence.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()) return false;
+        if(s.length()==0) return true;
+        
+        int subsequence = 0;
+        
+        for(int i=0; i<t.length(); i++){
+            if(subsequence<s.length()){
+                if(s.charAt(subsequence) == t.charAt(i))
+                    subsequence++;
+            }
+        }
+        return subsequence == s.length();
+    }
+}
