1283a4dfadb9236cb6ec04d43d22dca62944ef1c
Anuj <80551063+codeanuj@users.noreply.github.com>
2023-06-03 14:06:46 +0530

    Update TwoSum.java

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            int key = target-nums[i];
+            if(map.containsKey(key)){
+                return new int[]{i, map.get(key)};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
