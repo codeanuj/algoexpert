99364f9b35a771a2347b00f0530e45259441fd12
Anuj <80551063+codeanuj@users.noreply.github.com>
2023-07-14 12:58:14 +0530

    Update Solution.java

--- Array/Largest Range/Solution.java
+++ Array/Largest Range/Solution.java
@@ -1,1 +1,24 @@
 
+import java.util.*;
+
+class Program {
+  public static int[] largestRange(int[] array) {
+    // Write your code here.
+    
+    HashSet<Integer> set = new HashSet<Integer>();
+    for(int num:array) set.add(num);
+    int result[] = new int[2];
+    for(int i=0; i<array.length; i++){
+      if(set.contains(array[i]+1)) continue;
+      int max = array[i];
+      while(set.contains(--max));
+      max++;
+      //+1 because if input array is of [1,1]
+      if(result[1]-result[0]<array[i]-max+1){
+        result[0]=max;
+        result[1]=array[i];
+      }
+    }
+    return result;
+  }
+}
